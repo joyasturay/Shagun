2e8a1b6ac4e9bcdd709a2104485abe40d14fdedb
Joyastu Ray <joyasturay1@gmail.com>
2026-04-05 21:06:11 +0530

    FIX:AWS region

--- app/actions/upload.ts
+++ app/actions/upload.ts
@@ -4,7 +4,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { auth } from "../lib/auth";
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.NEXT_PUBLIC_AWS_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
